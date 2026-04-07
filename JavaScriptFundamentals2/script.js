07426e8ba98cf190a9be6106449a3dc566bfbae3
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-04-07 17:11:38 -0300

    use trict mode

---
+++ JavaScriptFundamentals2/script.js
@@ -0,0 +1,13 @@
+/*
+STRICT MODE
+    Always use strict mode in your JavaScript code.
+    It helps catch common coding mistakes and "unsafe"
+    actions, such as using undeclared variables.    
+*/
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
