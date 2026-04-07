831c6aba7561233aabfd46f37c00a3f86a4e33bd
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-04-07 15:06:40 -0300

    string literals

---
+++ JavaScriptFundamentals1/string-literals.js
@@ -0,0 +1,12 @@
+const firstName = 'Leonardo';
+const job = 'Developer';
+const birthYear = 1995;
+
+const leonardo = "I'm " + firstName +
+    ', a ' + (2026 - birthYear) +
+    'years old;';
+
+const leonardoNew = `I'm ${firstName}, a ${2026 - birthYear} years old`;
+
+console.log(leonardo);
+console.log(leonardoNew);
