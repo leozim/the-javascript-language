cc6e15e037b244fb3d61112b2fa5413bb8bea91a
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-04-07 17:30:32 -0300

    functions

--- JavaScriptFundamentals2/function-and-expressions.js
+++ JavaScriptFundamentals2/function-and-expressions.js
@@ -10,7 +10,13 @@ const func = function (birthYear) {
     return 2026 - birthYear;
 };
 
-const arroFunction = birthYear => 2026 - birthYear;
+const arrowFunction = birthYear => 2026 - birthYear;
 
 console.log(`My function expression is ${func(1995)}`);
-console.log(`My arrow function is ${arroFunction(1995)}`);
+console.log(`My arrow function is ${arrowFunction(1995)}`);
+
+const randomNumber = () => Number(Math.random().toFixed(2));
+
+const number = randomNumber();
+console.log(`My random number is ${number}`);
+console.log(number + 3);
